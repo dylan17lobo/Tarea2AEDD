59eda7014365f21bda9e03b25366b7ff9867c7ff
Dylan17Lobo <dylan17lobo@gmail.com>
2026-06-09 13:56:19 -0600

    creacion

---
+++ Tarea2_Dylan_C5G526_Samuel_C5F560/src/tarea2_dylan_c5g526_samuel_c5f560/Tarea2_Dylan_C5G526_Samuel_C5F560.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea2_dylan_c5g526_samuel_c5f560;
+
+/**
+ *
+ * @author Dylan Lobo
+ */
+public class Tarea2_Dylan_C5G526_Samuel_C5F560 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
